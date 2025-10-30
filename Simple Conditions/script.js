c2b12b5e6968bef845d18036b995ff9971d1a22c
Abdullah Nazir <abdullah03096344305@gmail.com>
2025-10-30 23:55:25 +0500

    first program done

--- Simple Conditions/script.js
+++ Simple Conditions/script.js
@@ -1,3 +1,16 @@
 //Take a number and print whether it’s positive, negative, or zero.
 
-let number = prompt("Enter a number");
+let number = Number(prompt("Enter a number"));
+console.log(number);
+if (isNaN(number)){
+    console.log("That's not a valid number!");
+}
+if (number > 0) {
+    console.log("the number is positive!")
+}
+else if (number < 0){
+    console.log("the number is negative");
+}
+else if (number == 0){
+    console.log("the number is zero");
+}
